598b077ca78496f9caf305fd448b288cb58fdea9
Snufkin0866 <snufkin0866@gmail.com>
2019-04-16 16:49:24 +0900

    typo

--- config_sample.py
+++ config_sample.py
@@ -5,5 +5,5 @@ CONTEST_START = datetime(2019,4,15)
 WEBHOOK_URL = "hogehoge"
 KEY = "hugahuga"
 SECRET = "piyopiyo"
-FUDNING_CURRENCIES = defaultdict(bool, {'JPY': True, 'BTC': False,
+FUNDING_CURRENCIES = defaultdict(bool, {'JPY': True, 'BTC': False,
                       'ETH': False, 'USD': False, 'QASH': False})
