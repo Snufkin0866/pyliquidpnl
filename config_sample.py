299c39f1bc57228f9da11e90d92eaad678222809
Snufkin0866 <snufkin0866@gmail.com>
2019-04-20 14:50:04 +0900

    add description

--- config_sample.py
+++ config_sample.py
@@ -1,15 +1,15 @@
 from collections import defaultdict
 from datetime import datetime
 from pytz import timezone
-
+# 大会スタート日時．現在4/15にしていますが，大会スタート前に5/1にしてください．
 CONTEST_START = timezone('Asia/Tokyo').localize(
     datetime(year=2019, month=4, day=15, hour=0, minute=0))
-INITIAL_CAPITAL = 10000
-USER_NAME = "hoge"
-PL_SERVER_URL = 'http://hoge'
-WEBHOOK_URL = "hogehoge"
-KEY = "hugahuga"
-SECRET = "piyopiyo"
+INITIAL_CAPITAL = 10000 # 大会の初期資金．現物を初期資金に含める場合は，JPY+現物の大会開始時の時価
+USER_NAME = "hoge" # 参加者の名前．わかりやすいもの．
+PL_SERVER_URL = 'http://hoge' # 大会参加者用部屋で案内します．
+WEBHOOK_URL = "hogehoge" # ご自身で設定した損益通知部屋のwebhookのURL
+KEY = "hugahuga" # LiquidのAPIキー
+SECRET = "piyopiyo" # LiquidのAPIシークレット
 # 自分が取引に使う基軸通貨と，BTCJPY以外の「現物」を取引する場合はその通貨を入れる．
 # レバレッジ取引の場合は，基軸通貨のみTrueで良いが，現物取引の場合は現物をTrueにする．
 # BTCJPYのレバレッジ取引→'JPY': Trueで他はFalse
